3ff98f17a6184ae317d26bff20fba13f2ce07cf9
Keith Jordan <shadow902@gmail.com>
2026-04-18 23:34:01 -0400

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 
 class IndexController extends Controller
 {
-    //
+    public function index()
+    {
+       return inertia('Index/Index', [
+        'message' => 'Hello from Laravel!',
+       ]);
+    }
+
+    public function show() 
+    {
+        return inertia('Index/Show');
+    }
 }
