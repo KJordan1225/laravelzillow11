9ea4b9efdf2a68ffe14d81e41c712047ffd67979
Keith Jordan <shadow902@gmail.com>
2026-04-21 02:14:25 -0400

    update authentication and security

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -10,17 +10,12 @@ class HandleInertiaRequests extends Middleware
     /**
      * The root template that's loaded on the first page visit.
      *
-     * @see https://inertiajs.com/server-side-setup#root-template
      * @var string
      */
     protected $rootView = 'app';
 
     /**
      * Determines the current asset version.
-     *
-     * @see https://inertiajs.com/asset-versioning
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
      */
     public function version(Request $request): ?string
     {
@@ -29,17 +24,23 @@ class HandleInertiaRequests extends Middleware
 
     /**
      * Defines the props that are shared by default.
-     *
-     * @see https://inertiajs.com/shared-data
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
      */
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
             'flash' => [
-                'success' => $request->session()->get('success')
-            ]
+                'success' => fn () => $request->session()->get('success'),
+            ],
+
+            'auth' => [
+                'user' => fn () => $request->user()
+                    ? [
+                        'id' => $request->user()->id,
+                        'name' => $request->user()->name,
+                        'email' => $request->user()->email,
+                    ]
+                    : null,
+            ],
         ]);
     }
 }
